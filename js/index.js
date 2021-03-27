c3d50cb7a5aede56ba009dda8941f0d295fdfded
Vince017 <rikkunanase7@gmail.com>
2021-03-28 00:04:01 +0800

    Initial commit

--- js/index.js
+++ js/index.js
@@ -7,19 +7,18 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
 var myIndex = 0;
-
-
 carousel();
 
 function carousel() {
   var i;
-  var x = document.getElementsByClassName("slider");
+  var x = document.getElementsByClassName("imgSlide");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}    
   x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 6000);    
-}
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+  }
