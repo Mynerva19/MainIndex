3ae25ad5f14122f6c20578787fddf3c13385aee4
Vince017 <rikkunanase7@gmail.com>
2021-03-27 22:03:02 +0800

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
-function main() {
 
-document.location = "php/index.php"
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+var myIndex = 0;
 
+
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slider");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 6000);    
 }
